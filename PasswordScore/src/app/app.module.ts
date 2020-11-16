a94a1fd56a265d668bcaf839e026e05ac383a74e
ahmed shora <ahmedalshora53@gmail.com>
2020-11-16 12:57:00 +0200

    improvments

--- PasswordScore/src/app/app.module.ts
+++ PasswordScore/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
